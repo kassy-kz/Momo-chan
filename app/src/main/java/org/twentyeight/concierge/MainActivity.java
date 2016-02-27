8ad00299ea0d702dfcc508edee6a7c7f3bcbc177
kassy_kz <kashimoto.kazutoshi@gmail.com>
2016-02-27 15:46:18 +0900

    Overlay周りのパーミッション解決

--- app/src/main/java/org/twentyeight/concierge/MainActivity.java
+++ app/src/main/java/org/twentyeight/concierge/MainActivity.java
@@ -1,7 +1,13 @@
 package org.twentyeight.concierge;
 
+import android.Manifest;
+import android.annotation.TargetApi;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.provider.Settings;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
@@ -14,6 +20,8 @@ import android.view.MenuItem;
 public class MainActivity extends AppCompatActivity {
 
     private static final String TAG = "ConciergeMainActivity";
+    private static final int RC_HANDLE_PERMISSION = 1;
+    private static final int REQUEST_CODE = 2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,16 +39,70 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+
         // NotificationListenerServiceを立ち上げる
-        Intent serviceIntent = new Intent(this, MyNotificationListenerService.class);
-        startService(serviceIntent);
+//        Intent serviceIntent = new Intent(this, MyNotificationListenerService.class);
+//        startService(serviceIntent);
+    }
 
-        // ConciergeService 起動
-        Log.d(TAG,"起動");
+    @Override
+    protected void onResume() {
+        super.onResume();
+        // 一番最初にパーミッションたずねてしまおう
+        // まだパーミッションを得ていない
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.M && !checkPermission()) {
+            Log.i(TAG, "not granted yet");
+//            // ダイアログを出してパーミッション許可を取りに行く
+            checkDrawOverlayPermission();
+        }
+        // 既にパーミッションを得ている or M未満だから関係ない場合
+        else {
+            Log.i(TAG, "already granted");
+            // ConciergeService 起動
+            Log.d(TAG,"起動");
+            startService(new Intent(MainActivity.this, ConciergeService.class));
+        }
+    }
+
+    /**
+     * オーバーレイの許可を取りに行く
+     */
+    @TargetApi(Build.VERSION_CODES.M)
+    public void checkDrawOverlayPermission() {
+        if (!Settings.canDrawOverlays(this)) {
+            Intent intent = new Intent(Settings.ACTION_MANAGE_OVERLAY_PERMISSION,
+                    Uri.parse("package:" + getPackageName()));
+            startActivityForResult(intent, REQUEST_CODE);
+        }
+    }
+
+    /**
+     * パーミッションを尋ねた結果
+     * @param requestCode
+     * @param permissions
+     * @param grantResults
+     */
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        Log.i(TAG, "onRequestPermissionsResult");
+        Log.d(TAG, "起動");
         startService(new Intent(MainActivity.this, ConciergeService.class));
-//        Intent intent = new Intent(this, ConciergeService.class);
-//        startService(intent);
+    }
 
+    /**
+     * アプリに必要なパーミッションを尋ねる
+     * @return true if yet granted, false if not granted
+     */
+    private boolean checkPermission() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            if (checkSelfPermission(Manifest.permission.SYSTEM_ALERT_WINDOW) != PackageManager.PERMISSION_GRANTED) {
+                Log.i(TAG,"permission ng");
+                return false;
+            }
+        }
+        Log.i(TAG,"permission ok");
+        return true;
     }
 
     @Override
