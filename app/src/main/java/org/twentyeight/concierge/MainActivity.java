20f05a5de1b7bfa8e8dadc2bf4f9c2eb0cdfb30e
Y.KEISUKE <yamagutikeisuke@gmail.com>
2016-02-27 14:40:49 +0900

    ConciergeService起動のコードをActivityに追加

--- app/src/main/java/org/twentyeight/concierge/MainActivity.java
+++ app/src/main/java/org/twentyeight/concierge/MainActivity.java
@@ -1,5 +1,6 @@
 package org.twentyeight.concierge;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -26,6 +27,11 @@ public class MainActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+
+        // ConciergeService 起動
+        Intent intent = new Intent(this, ConciergeService.class);
+        startService(intent);
+
     }
 
     @Override
