0c0e96153c2bb24d61f15f2d0d343088659eeae8
kassy_kz <kashimoto.kazutoshi@gmail.com>
2016-02-27 17:27:32 +0900

    ドラッグの範囲拡大（全画面のタッチイベント持って行ってしまった）

--- app/src/main/java/org/twentyeight/concierge/ConciergeService.java
+++ app/src/main/java/org/twentyeight/concierge/ConciergeService.java
@@ -34,8 +34,8 @@ public class ConciergeService extends Service {
 
         // 重ね合わせするViewの設定を行う
         WindowManager.LayoutParams params = new WindowManager.LayoutParams(
-                WindowManager.LayoutParams.WRAP_CONTENT,
-                WindowManager.LayoutParams.WRAP_CONTENT,
+                WindowManager.LayoutParams.MATCH_PARENT,
+                WindowManager.LayoutParams.MATCH_PARENT,
                 WindowManager.LayoutParams.TYPE_SYSTEM_ALERT,
                 WindowManager.LayoutParams.FLAG_NOT_TOUCH_MODAL|
                 WindowManager.LayoutParams.FLAG_WATCH_OUTSIDE_TOUCH,
