87a7cbd68aa2d519e65d0f7841bcdbe555815c49
Y.KEISUKE <yamagutikeisuke@gmail.com>
2016-02-27 21:26:47 +0900

    キャラ非表示サンプル実装（非表示とならない）

--- app/src/main/java/org/twentyeight/concierge/ConciergeService.java
+++ app/src/main/java/org/twentyeight/concierge/ConciergeService.java
@@ -9,6 +9,7 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.WindowManager;
+import android.widget.ImageView;
 
 /**
  * Created by YKEI on 2016/02/27.
@@ -48,6 +49,11 @@ public class ConciergeService extends Service {
         // レイアウトファイルから重ね合わせするViewを作成する
         view = layoutInflater.inflate(R.layout.overlay, null);
 
+
+        testChange();
+
+
+
         Log.d(TAG, "onStart start 040");
 
         // Viewを画面上に重ね合わせする
@@ -56,6 +62,66 @@ public class ConciergeService extends Service {
         Log.d(TAG, "onStart end");
 
     }
+
+    /**
+     * 表情を切り替える
+     * @param type 0: １: 2:
+     */
+    private void chenge(int type){
+
+        // typeに応じて呼び出すメソッドを変更する
+        switch (type){
+            case 0:
+
+                this._01();
+
+                break;
+            case 1:
+                Log.d(TAG, "1");
+                break;
+            case 2:
+                Log.d(TAG, "2");
+                break;
+            case 3:
+                Log.d(TAG, "3");
+                break;
+            case 4:
+                Log.d(TAG, "4");
+                break;
+            default:
+                Log.d(TAG, "ERROR : " + type);
+                break;
+        }
+
+    }
+
+    /**
+     * 立ってる。アイドル状態に変更する
+     */
+    private void _01(){
+
+        // MEMO:サンプル実装
+        LayoutInflater inflater = (LayoutInflater) getSystemService(LAYOUT_INFLATER_SERVICE);
+        View view = inflater.inflate(R.layout.overlay, null);
+        ImageView image = (ImageView) view.findViewById(R.id.unity_chan_walk);
+        image.setVisibility(View.INVISIBLE);
+
+    }
+
+    public void testChange(){
+
+        // Success
+        this.chenge(0);
+        this.chenge(1);
+        this.chenge(2);
+        this.chenge(3);
+        this.chenge(4);
+
+        // Faild
+        this.chenge(100);
+
+    }
+
 
     @Override
     public void onCreate() {
