c4d93e8691c5638f81ce107812f95b9912db477b
kassy_kz <kashimoto.kazutoshi@gmail.com>
2016-03-04 22:25:11 +0900

    軽くリファクタリング（非使用メソッド削除）

--- app/src/main/java/org/twentyeight/momo/ConciergeService.java
+++ app/src/main/java/org/twentyeight/momo/ConciergeService.java
@@ -60,15 +60,6 @@ public class ConciergeService extends Service {
     private String mBeforeApp = "";
     private Timer mImageChangeTimer;
 
-    // 現在表示したいID(R.id.hoge)
-    private int currentImageViewId = 0;
-
-    // アニメーションの実行回数
-    private int count = 0;
-
-    // ↓ これを変えるとキャラがかわります！！
-    // 現在実行すべきアニメーションの種類番号
-    private String currentType = "1"; // 1〜17にすると変わります！
     private int currentTypeInt = 1; // 1〜17にすると変わります！
 
     // タイマー（定期実行関係）
@@ -77,9 +68,6 @@ public class ConciergeService extends Service {
 
     // アニメデータのサイクル
     private final int MS = 500;
-
-    // アニメデータ
-    AnimationData anime = new AnimationData();
 
     // 音声再生
     MediaPlayer mMediaPlayer = null;
@@ -133,97 +121,6 @@ public class ConciergeService extends Service {
     }
 
     /**
-     * アニメデータ保持クラス
-     */
-    public static class AnimationData{
-
-        private HashMap<String,String[][]> animationData = new HashMap<String,String[][]>();
-        private final String[] INIT_D = {"", "", ""};
-        private final String[][] INIT_DATA = {{"", "", ""}};
-
-        AnimationData(){
-
-            // 立っている
-            this.animationData.put("1", new String[][]{{String.valueOf(R.drawable.idle_r1),"700","900"}, {String.valueOf(R.drawable.idle_r2),"700","900"}});
-
-            // 右
-            this.animationData.put("2", new String[][]{{String.valueOf(R.drawable.walkright_r1),"700","900"}, {String.valueOf(R.drawable.walkright_r2),"700","900"}, {String.valueOf(R.drawable.walkright_r3),"700","900"}});
-
-            // 後ろ
-            this.animationData.put("3", new String[][]{{String.valueOf(R.drawable.walkback_r1),"700","900"}, {String.valueOf(R.drawable.walkback_r2),"700","900"}, {String.valueOf(R.drawable.walkback_r3),"700","900"}});
-
-            // 左
-            this.animationData.put("4", new String[][]{{String.valueOf(R.drawable.walkleft_r1),"700","900"}, {String.valueOf(R.drawable.walkleft_r2),"700","900"}, {String.valueOf(R.drawable.walkleft_r3),"700","900"}});
-
-            // 正面
-            this.animationData.put("5", new String[][]{{String.valueOf(R.drawable.walkfront_r1),"700","900"}, {String.valueOf(R.drawable.walkfront_r2),"700","900"}, {String.valueOf(R.drawable.walkfront_r3),"700","900"}});
-
-            // スマイルA(座り)
-            this.animationData.put("6", new String[][]{{String.valueOf(R.drawable.smile_a_r1),"700","700"}, {String.valueOf(R.drawable.smile_a_r2),"700","700"}, {String.valueOf(R.drawable.smile_a_r3),"700","700"}});
-
-            // スマイルB(座り)
-            this.animationData.put("7", new String[][]{{String.valueOf(R.drawable.smile_b_r1),"700","700"}, {String.valueOf(R.drawable.smile_b_r2),"700","700"}});
-
-            // スマイルC(座り)
-            this.animationData.put("8", new String[][]{{String.valueOf(R.drawable.smile_c_r1),"700","700"}, {String.valueOf(R.drawable.smile_c_r2),"700","700"}});
-
-            // スマイルD(座り)
-            this.animationData.put("9", new String[][]{{String.valueOf(R.drawable.smile_d_r1),"700","700"}, {String.valueOf(R.drawable.smile_d_r2),"700","700"},{String.valueOf(R.drawable.smile_d_r3),"700","700"}});
-
-            // びっくり
-            this.animationData.put("10", new String[][]{{String.valueOf(R.drawable.surprise_r1),"700","700"}, {String.valueOf(R.drawable.surprise_r2),"700","700"},{String.valueOf(R.drawable.surprise_r3),"700","700"}});
-
-            // はてな
-            this.animationData.put("11", new String[][]{{String.valueOf(R.drawable.question_r),"700","700"}});
-
-            // 困りA
-            this.animationData.put("12", new String[][]{{String.valueOf(R.drawable.trouble_a_r),"700","700"}});
-
-            // 困りB
-            this.animationData.put("13", new String[][]{{String.valueOf(R.drawable.trouble_b_r1),"700","700"}, {String.valueOf(R.drawable.trouble_b_r2),"700","700"},{String.valueOf(R.drawable.trouble_b_r3),"700","700"}});
-
-            // 困りC
-            this.animationData.put("14", new String[][]{{String.valueOf(R.drawable.trouble_c_r1),"700","700"}, {String.valueOf(R.drawable.trouble_c_r2),"700","700"},{String.valueOf(R.drawable.trouble_c_r3),"700","700"}});
-
-            // 渋い顔
-            this.animationData.put("15", new String[][]{{String.valueOf(R.drawable.bitter_r1),"700","700"}, {String.valueOf(R.drawable.bitter_r2),"700","700"},{String.valueOf(R.drawable.bitter_r3),"700","700"}});
-
-            // 睡眠
-            this.animationData.put("16", new String[][]{{String.valueOf(R.drawable.sleep_r2),"700","700"}, {String.valueOf(R.drawable.sleep_r2),"700","700"}});
-
-            // 目覚め
-            this.animationData.put("17", new String[][]{{String.valueOf(R.drawable.awake_r1),"700","700"}, {String.valueOf(R.drawable.awake_r2),"700","700"}});
-
-            // 口パク
-            this.animationData.put("18", new String[][]{{String.valueOf(R.drawable.smile_d_r1),"700","700"}, {String.valueOf(R.drawable.smile_a_r2),"700","700"}});
-            
-        }
-
-        /**
-         *
-         * @param key
-         * @return String[]
-         */
-        public String[][] get(String key){
-            if(this.animationData.get(key) != null) {
-                return this.animationData.get(key);
-            }else{
-                return INIT_DATA;
-            }
-        }
-
-        // 現在有効なanimation(keyで指定)のnumber番目の配列を取得する
-        public String[] getCurrentChild(String key, int number){
-            if(this.animationData.get(key) != null) {
-                return this.animationData.get(key)[number];
-            }else{
-                return INIT_D;
-            }
-        }
-
-    }
-
-    /**
      * onStartCommand
      * @param intent
      * @param flags
@@ -242,45 +139,6 @@ public class ConciergeService extends Service {
             }
         }, 10000, 10000);
         return START_STICKY;
-    }
-
-    /**
-     * タイマー
-     * アニメーションをする。
-     * 本メソッドでは、定期実行のみおこなう。
-     * 実処理自体は、別メソッドでおこなっている。
-     */
-    public void setTimer(){
-        mTimer = new Timer(true);
-        mTimer.schedule( new TimerTask(){
-            @Override
-            public void run() {
-                mHandler.post(new Runnable() {
-                    public void run() {
-                        // 設定されている種類に応じた画面遷移(アニメーション(=画像差し替え))
-                        update();
-                    }
-                });
-            }
-        }, 1000, MS);
-    }
-
-    private void update(){
-
-        // 表示すべき項目の表示
-        ImageView currentImage = (ImageView) view.findViewById(currentImageViewId);
-
-        // コマ数別
-        if(anime.get(currentType).length == 2){
-            currentImage.setImageResource(Integer.parseInt(
-                    anime.getCurrentChild(currentType, count % 2)[0]));
-        }else if(anime.get(currentType).length == 3){
-            currentImage.setImageResource(Integer.parseInt(
-                    anime.getCurrentChild(currentType, count % 3)[0]));
-        }
-
-        count++;
-
     }
 
     @Override
@@ -352,12 +210,7 @@ public class ConciergeService extends Service {
             }
         }, 100, 100);
 
-        // TextToSpeechオブジェクトの生成
-//        mTts = new TextToSpeech(this, this);
-
         // 定期実行のタイマー設定
-//        this.currentImageViewId = R.id.characterImageView;
-//        setTimer();
         setImageMap();
         mMainImageView = (ImageView) view.findViewById(R.id.characterImageView);
         setImageChangeTimer();
@@ -552,10 +405,6 @@ public class ConciergeService extends Service {
                 } else {
                     mWalkTimer.cancel();
                     changeAnimeType(MOMO_STAND);
-                    Random random = new Random();
-                    currentTypeInt = random.nextInt(13) + 6;
-                    Log.i(TAG, "random type : " + currentTypeInt);
-                    currentType = currentTypeInt + "";
                 }
             }
         }, 16, 16);
@@ -581,30 +430,6 @@ public class ConciergeService extends Service {
                 }
             }
         });
-    }
-
-    private void speechVoice(String str) {
-        if (mMediaPlayer == null) {
-            mMediaPlayer = MediaPlayer.create(this, R.raw.sample1);
-            mMediaPlayer.start();
-            return;
-        }
-        if (mMediaPlayer.isPlaying()) {
-            mMediaPlayer.stop();
-//            mMediaPlayer.prepare();
-        }
-        Log.i(TAG, "speech voice");
-        mMediaPlayer = MediaPlayer.create(this, R.raw.sample1);
-        currentTypeInt = 18;
-        currentType = "18";
-        mMediaPlayer.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
-            @Override
-            public void onCompletion(MediaPlayer mp) {
-                currentTypeInt = 0;
-                currentType = "0";
-            }
-        });
-        mMediaPlayer.start();
     }
 
     private void speechVoice(int resId) {
