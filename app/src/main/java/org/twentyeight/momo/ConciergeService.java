90f646993d2c1ba3170de7df30a6778688d1d0a0
kassy_kz <kashimoto.kazutoshi@gmail.com>
2016-03-04 22:22:22 +0900

    アニメーションを全種類実装（全種呼び出しはまだしていない）

--- app/src/main/java/org/twentyeight/momo/ConciergeService.java
+++ app/src/main/java/org/twentyeight/momo/ConciergeService.java
@@ -24,9 +24,31 @@ import java.util.HashMap;
  */
 public class ConciergeService extends Service {
     private static final long CYCLE_ANIME = 500;
-    private static final int WALK_COUNT_MAX = 120;
+    private static final int WALK_COUNT_MAX = 180; // 2秒
 
     private static final String TAG = "ConciergeService";
+
+    /**
+     * ももちゃんのアニメーションの種別を表す定数
+     */
+    private static final Integer MOMO_STAND = 0;
+    private static final Integer MOMO_WALK_L = 1;
+    private static final Integer MOMO_WALK_R = 2;
+    private static final Integer MOMO_WALK_U = 3;
+    private static final Integer MOMO_WALK_D = 4;
+    private static final Integer MOMO_SMILE_A = 5;
+    private static final Integer MOMO_SMILE_B = 6;
+    private static final Integer MOMO_SMILE_C = 7;
+    private static final Integer MOMO_SMILE_D = 8;
+    private static final Integer MOMO_SURPRISE = 9;
+    private static final Integer MOMO_QUESTION = 10;
+    private static final Integer MOMO_TROUBLE_A = 11;
+    private static final Integer MOMO_TROUBLE_B = 12;
+    private static final Integer MOMO_BITTER = 13;
+    private static final Integer MOMO_SLEEP = 14;
+    private static final Integer MOMO_CRY = 15;
+    private static final Integer MOMO_TALK = 16;
+
     private View view;
     private WindowManager wm;
     private WindowManager.LayoutParams params;
@@ -67,11 +89,47 @@ public class ConciergeService extends Service {
     private int mAnimeType = 0;
     private int mAnimeCount = 0;
 
-    private HashMap<Integer, int[]> animeImageMap = new HashMap<>();
+    private HashMap<Integer, int[]> mAnimeImageMap = new HashMap<>();
 
+    /**
+     * アニメーションのパターンの定義
+     */
     private void setImageMap() {
-        animeImageMap.put(0, new int[]{R.drawable.idle_r1, R.drawable.idle_r2});
-        animeImageMap.put(1, new int[]{R.drawable.walkright_r1, R.drawable.walkright_r2, R.drawable.walkright_r1, R.drawable.walkright_r3});
+        // 素立ち
+        mAnimeImageMap.put(MOMO_STAND, new int[]{R.drawable.idle_r1, R.drawable.idle_r2});
+        // 歩き左
+        mAnimeImageMap.put(MOMO_WALK_L, new int[]{R.drawable.walkright_r1, R.drawable.walkright_r2, R.drawable.walkright_r1, R.drawable.walkright_r3});
+        // 歩き右
+        mAnimeImageMap.put(MOMO_WALK_R, new int[]{R.drawable.walkleft_r1, R.drawable.walkleft_r2, R.drawable.walkleft_r1, R.drawable.walkleft_r3});
+        // 歩き上
+        mAnimeImageMap.put(MOMO_WALK_U, new int[]{R.drawable.walkback_r1, R.drawable.walkback_r2, R.drawable.walkback_r1, R.drawable.walkback_r3});
+        // 歩き下
+        mAnimeImageMap.put(MOMO_WALK_D, new int[]{R.drawable.walkfront_r1, R.drawable.walkfront_r2, R.drawable.walkfront_r1, R.drawable.walkfront_r3});
+        // スマイルA座り
+        mAnimeImageMap.put(MOMO_SMILE_A, new int[]{R.drawable.smile_a_r1, R.drawable.smile_a_r2, R.drawable.smile_a_r1, R.drawable.smile_a_r3});
+        // スマイルB座り
+        mAnimeImageMap.put(MOMO_SMILE_B, new int[]{R.drawable.smile_b_r1, R.drawable.smile_b_r2});
+        // スマイルC座り
+        mAnimeImageMap.put(MOMO_SMILE_C, new int[]{R.drawable.smile_c_r1, R.drawable.smile_c_r2});
+        // スマイルD座り
+        mAnimeImageMap.put(MOMO_SMILE_D, new int[]{R.drawable.smile_d_r1, R.drawable.smile_d_r2, R.drawable.smile_d_r1, R.drawable.smile_d_r3});
+        // びっくり
+        mAnimeImageMap.put(MOMO_SURPRISE, new int[]{R.drawable.surprise_r1, R.drawable.surprise_r2, R.drawable.surprise_r1, R.drawable.surprise_r3});
+        // はてな
+        mAnimeImageMap.put(MOMO_QUESTION, new int[]{R.drawable.question_r});
+        // 困りA
+        mAnimeImageMap.put(MOMO_TROUBLE_A, new int[]{R.drawable.trouble_b_r1, R.drawable.trouble_b_r2, R.drawable.trouble_b_r1, R.drawable.trouble_b_r3});
+        // 困りB
+        mAnimeImageMap.put(MOMO_TROUBLE_B, new int[]{R.drawable.trouble_c_r1, R.drawable.trouble_c_r2, R.drawable.trouble_c_r1, R.drawable.trouble_c_r3});
+        // 渋い顔
+        mAnimeImageMap.put(MOMO_BITTER, new int[]{R.drawable.bitter_r1, R.drawable.bitter_r2, R.drawable.bitter_r1, R.drawable.bitter_r3});
+        // 眠り
+        mAnimeImageMap.put(MOMO_SLEEP, new int[]{R.drawable.sleep_r1, R.drawable.sleep_r2});
+        // なき
+        mAnimeImageMap.put(MOMO_CRY, new int[]{R.drawable.cry_r1, R.drawable.cry_r2});
+        // 口パク
+        mAnimeImageMap.put(MOMO_TALK, new int[]{R.drawable.smile_d_r1, R.drawable.smile_a_r1});
+
     }
 
     /**
@@ -165,7 +223,13 @@ public class ConciergeService extends Service {
 
     }
 
-
+    /**
+     * onStartCommand
+     * @param intent
+     * @param flags
+     * @param startId
+     * @return
+     */
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
         super.onStartCommand(intent, flags, startId);
@@ -355,7 +419,7 @@ public class ConciergeService extends Service {
      * ももちゃんの画像を差し替える
      */
     private void changeMainImage() {
-        int[] animeImages = animeImageMap.get(mAnimeType);
+        int[] animeImages = mAnimeImageMap.get(mAnimeType);
         int imageId = animeImages[mAnimeCount % animeImages.length];
         mMainImageView.setImageResource(imageId);
         mAnimeCount++;
@@ -366,27 +430,25 @@ public class ConciergeService extends Service {
      * @param type
      */
     private void changeAnimeType(int type) {
+        // 変化がないならそのまま
+        if (type == mAnimeType) {
+            return;
+        }
         mAnimeCount = 0;
         mAnimeType = type;
         setImageChangeTimer();
     }
 
+    /**
+     * ももちゃんをタッチした時のリスナー
+     */
     private class DragViewListener implements View.OnTouchListener {
-        // ドラッグ対象のView
-        private View dragView;
-        // ドラッグ中に移動量を取得するための変数
-        private int oldx = 0;
-        private int oldy = 0;
-
         private int dragStartX = 0;
         private int dragStartY = 0;
         private int downX = 0;
         private int downY = 0;
 
         public DragViewListener(View dragView) {
-            this.dragView = dragView;
-            oldx = params.x;
-            oldy = params.y;
         }
 
         @Override
@@ -398,6 +460,7 @@ public class ConciergeService extends Service {
 
             // 画像と重ならなければスルーする
             switch (event.getAction()) {
+                // おした時
                 case MotionEvent.ACTION_DOWN:
                     changeAnimeType(0);
 
@@ -407,7 +470,10 @@ public class ConciergeService extends Service {
                     dragStartY = y;
 //                    Log.i(TAG, "first params " + params.x + ","+params.y);
                     break;
+
+                // ドラッグしたとき
                 case MotionEvent.ACTION_MOVE:
+                    changeAnimeType(MOMO_SURPRISE);
                     // 今回イベントでのView移動先の位置
                     int deltaX = x - dragStartX;
                     int deltaY = y - dragStartY;
@@ -418,6 +484,8 @@ public class ConciergeService extends Service {
                     dragStartY = y;
 //                    Log.i(TAG, "update params " + params.x + ","+params.y);
                     break;
+
+                // 離した時
                 case MotionEvent.ACTION_UP:
                     // ドラッグをしていない時
                     changeAnimeType(1);
@@ -442,11 +510,12 @@ public class ConciergeService extends Service {
                                 break;
                         }
                     }
+                    // ドラッグをしたとき
+                    else {
+                        changeAnimeType(MOMO_TROUBLE_A);
+                    }
                     break;
             }
-            // 今回のタッチ位置を保持
-            oldx = x;
-            oldy = y;
             // イベント処理完了
             return true;
         }
@@ -463,15 +532,15 @@ public class ConciergeService extends Service {
         final double radian = Math.toRadians(degrees);
 
         if (degrees < 45) {
-            currentType = "4";
+            changeAnimeType(MOMO_WALK_R);
         } else if (degrees < 135) {
-            currentType = "5";
+            changeAnimeType(MOMO_WALK_D);
         } else if (degrees < 225) {
-            currentType = "2";
+            changeAnimeType(MOMO_WALK_L);
         } else if (degrees < 315) {
-            currentType = "3";
+            changeAnimeType(MOMO_WALK_U);
         } else {
-            currentType = "4";
+            changeAnimeType(MOMO_WALK_R);
         }
 
         mWalkTimer.schedule(new TimerTask() {
@@ -482,6 +551,7 @@ public class ConciergeService extends Service {
                     mWalkCounter++;
                 } else {
                     mWalkTimer.cancel();
+                    changeAnimeType(MOMO_STAND);
                     Random random = new Random();
                     currentTypeInt = random.nextInt(13) + 6;
                     Log.i(TAG, "random type : " + currentTypeInt);
